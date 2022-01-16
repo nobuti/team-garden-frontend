abbe4eec1a342dfe56421db3e33e57552f521de8
nobuti <buti@nobuti.com>
2022-01-16 11:40:33 +0100

    Empty results state for list
    Fine tune whitespacing

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 
 import styles from './styles.module.css';
 
-const List = ({ data, render, metakey }) => (
-  <ul className={styles.list}>
-    {data.map((d) => (
-      <li key={d[metakey]}>{render(d)}</li>
-    ))}
-  </ul>
-);
+const List = ({ data, render, metakey }) => {
+  if (data.length === 0) {
+    return (
+      <div className={styles.empty}>
+        There are no results to show. Take the time and send an insteresting
+        resource for you and your team!
+      </div>
+    );
+  }
+  return (
+    <ul className={styles.list}>
+      {data.map((d) => (
+        <li key={d[metakey]}>{render(d)}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default List;
